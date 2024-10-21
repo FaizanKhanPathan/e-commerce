9d140bec114f17a0ad3ff816574d57839be7f2a3
Faizankhan-786 <152836000+Faizankhan-786@users.noreply.github.com>
2024-10-21 21:19:20 +0530

    new

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "dko703bey",
-  api_key: "158667894646749",
-  api_secret: "WoFNwFXO1FXdYKakUPCu4kloiyA",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new multer.memoryStorage();
